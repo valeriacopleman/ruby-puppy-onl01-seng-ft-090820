a581b2f6a5bafb976151022d0b946fb018937a63
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-19 06:06:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+  
+  @@all = [] 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    save 
+  end 
+  
+  def self.all
+    @@all
+  end
+ 
+ def save
+   @@all << self
+ end
+ 
+ def self.clear_all
+   @@all = []
+ end
+ 
+  def self.print_all
+    #binding.pry
+    @@all.each do |single_dog|
+      puts single_dog.name
+    end
+  end
+  
+end
