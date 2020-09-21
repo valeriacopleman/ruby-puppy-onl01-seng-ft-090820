e30a813e3690d4fe6bcd4209997921960c53b05a
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 04:17:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,15 +8,20 @@ class Dog
   
   def initialize(name)
     @name = name
-    save
+    save(self)
     end 
   
   def self.all
     @@all
   end
  
+<<<<<<< HEAD
  def save
    @@all << self
+=======
+ def save(new_dog)
+   @@all << new_dog
+>>>>>>> 0bd51b48c16a12389595e99d117b799e2f591ff1
  end
    
  def self.clear_all
